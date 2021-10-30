029ba31df0cf40901fc3e0d391d336643087e450
gerrypf <73151831+gerrypf@users.noreply.github.com>
2021-10-30 21:21:04 +0700

    Update water7.sh

--- water7.sh
+++ water7.sh
@@ -5,10 +5,10 @@ apt-get update
 apt-get update
 apt-get install bind9 -y
 echo '
-zone "franky.t07.com" {
+zone "franky.t05.com" {
         type slave;
-        masters { 10.45.2.2; }; // Masukan IP EniesLobby tanpa tanda petik
-        file "/var/lib/bind/franky.t07.com";
+        masters { 10.44.2.2; }; // Masukan IP EniesLobby tanpa tanda petik
+        file "/var/lib/bind/franky.t05.com";
 };
 ' > /etc/bind/named.conf.local
 service bind9 restart
@@ -24,47 +24,47 @@ options {
 };
 " > /etc/bind/named.conf.options
 echo '
-zone "franky.t07.com" {
+zone "franky.t05.com" {
         type slave;
-        masters { 10.45.2.2; }; // Masukan IP EniesLobby tanpa tanda petik
-        file "/var/lib/bind/franky.t07.com";
+        masters { 10.44.2.2; }; // Masukan IP EniesLobby tanpa tanda petik
+        file "/var/lib/bind/franky.t05.com";
 };
-zone "mecha.franky.t07.com"{
+zone "mecha.franky.t05.com"{
         type master;
-        file "/etc/bind/sunnygo/mecha.franky.t07.com";
+        file "/etc/bind/sunnygo/mecha.franky.t05.com";
 };
 '> /etc/bind/named.conf.local
 mkdir /etc/bind/sunnygo
 echo "
 \$TTL    604800
-@       IN      SOA     mecha.franky.t07.com. root.mecha.franky.t07.com. (
+@       IN      SOA     mecha.franky.t05.com. root.mecha.franky.t05.com. (
                         2021100401      ; Serial
                         604800         ; Refresh
                         86400         ; Retry
                         2419200         ; Expire
                         604800 )       ; Negative Cache TTL
 ;
-@               IN      NS      mecha.franky.t07.com.
-@               IN      A       10.45.2.4       ;ip skypie
-www             IN      CNAME   mecha.franky.t07.com.
-" > /etc/bind/sunnygo/mecha.franky.t07.com
+@               IN      NS      mecha.franky.t05.com.
+@               IN      A       10.44.2.4       ;ip skypie
+www             IN      CNAME   mecha.franky.t05.com.
+" > /etc/bind/sunnygo/mecha.franky.t05.com
 service bind9 restart
 
 
 # Nomor 7
 echo "
 \$TTL    604800
-@       IN      SOA     mecha.franky.t07.com. root.mecha.franky.t07.com. (
+@       IN      SOA     mecha.franky.t05.com. root.mecha.franky.t07.com. (
                         2021100401      ; Serial
                         604800         ; Refresh
                         86400         ; Retry
                         2419200         ; Expire
                         604800 )       ; Negative Cache TTL
 ;
-@               IN      NS      mecha.franky.t07.com.
-@               IN      A       10.45.2.4       ;ip skypie
-www             IN      CNAME   mecha.franky.t07.com.
-general         IN      A       10.45.2.4       ;IP skypie
-www.general     IN      CNAME   general.mecha.franky.t07.com.
-" > /etc/bind/sunnygo/mecha.franky.t07.com
+@               IN      NS      mecha.franky.t05.com.
+@               IN      A       10.44.2.4       ;ip skypie
+www             IN      CNAME   mecha.franky.t05.com.
+general         IN      A       10.44.2.4       ;IP skypie
+www.general     IN      CNAME   general.mecha.franky.t05.com.
+" > /etc/bind/sunnygo/mecha.franky.t05.com
 service bind9 restart
