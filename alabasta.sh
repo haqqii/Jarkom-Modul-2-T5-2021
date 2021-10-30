5bcc323b1317e92cf766ffa32db296588fec0302
gerrypf <73151831+gerrypf@users.noreply.github.com>
2021-10-30 21:08:19 +0700

    Create alabasta.sh

---
+++ alabasta.sh
@@ -0,0 +1,10 @@
+echo "nameserver 192.168.122.1" > /etc/resolv.conf
+apt-get update
+apt-get install dnsutils -y
+apt-get install lynx -y
+# nomor 5
+echo "
+nameserver 10.45.2.2
+nameserver 10.45.2.3
+nameserver 10.45.2.4
+" > /etc/resolv.conf
